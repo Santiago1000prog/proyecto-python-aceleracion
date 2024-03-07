22714be110ecc2116b9bc78f4a478b0a2d385353
Santi Emanuel <santiemanuel@gmail.com>
2024-03-07 17:33:11 -0300

    Borramos un ejercicio y descomentamos el otro

--- ejercicio-2.py
+++
@@ -1,8 +0,0 @@
-cadena = "Hola mundo"
-slicing = cadena[2:7]  # "la mu"
-nueva = slicing.upper()  # "LA MU"
-print(nueva)
-print(f"<{cadena[4]}>")
-
-# H o l a [ ] m u n d o
-# 0 1 <2 3  4  5 6> => la mu
